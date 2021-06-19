70af3bdf73797625c3276ef60fba575594457a3c
Mariam Mekhrishvili <mariamekh@github.com>
2021-06-19 16:53:30 +0400

    feat: implement logic of favorites page component

--- src/components/Favorite/Favorite.jsx
+++ src/components/Favorite/Favorite.jsx
@@ -1,11 +1,22 @@
 import styles from "./Favorite.module.css";
 import icon from "./img/bookmark.svg";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+
 const Favorite = () => {
+  const [count, setCount] = useState();
+  const storeDate = useSelector((state) => state.favoriteReducer);
+
+  useEffect(() => {
+    const length = Object.keys(storeDate).length;
+    length.toString().length > 2 ? setCount("...") : setCount(length);
+  }, [storeDate]);
+
   return (
     <div className={styles.container}>
       <Link to="/favorites">
-        <span className={styles.counter}>0</span>
+        <span className={styles.counter}>{count}</span>
         <img className={styles.icon} src={icon} />
       </Link>
     </div>
--- src/containers/FavoritesPage/FavoritesPage.jsx
+++ src/containers/FavoritesPage/FavoritesPage.jsx
@@ -1,12 +1,34 @@
 import { useSelector } from "react-redux";
 import styles from "./FavoritesPage.module.css";
+import PeopleList from "@components/PeoplePage/PeopleList";
+import { useEffect, useState } from "react";
 
 const FavoritesPage = () => {
+  const [people, setPeople] = useState([]);
   const storeData = useSelector((state) => state.favoriteReducer);
-  console.log(storeData);
+
+  useEffect(() => {
+    const arr = Object.entries(storeData);
+
+    if (arr.length) {
+      const res = arr.map((el) => {
+        return {
+          id: el[0],
+          ...el[1],
+        };
+      });
+      setPeople(res);
+    }
+  }, []);
+
   return (
     <>
-      favorites<h1>favs</h1>
+      <h1 className="header__text">Favorites Page</h1>
+      {people.length ? (
+        <PeopleList people={people} />
+      ) : (
+        <h2 className={styles.comment}>No data</h2>
+      )}
     </>
   );
 };
