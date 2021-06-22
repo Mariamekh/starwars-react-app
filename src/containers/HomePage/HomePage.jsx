f247700e82a9490ad214e2e8ebaafa86b35d2bb7
Mariam Mekhrishvili <mariamekh@github.com>
2021-06-22 19:22:59 +0400

    fix: change homepage text

--- src/containers/HomePage/HomePage.jsx
+++ src/containers/HomePage/HomePage.jsx
@@ -5,7 +5,7 @@ import styles from "./HomePage.module.css";
 const HomePage = () => {
   return (
     <>
-      <h1 className="header__text">Home Page</h1>
+      <h1 className="header__text">Choose your side</h1>
       <ChooseSide />
     </>
   );
